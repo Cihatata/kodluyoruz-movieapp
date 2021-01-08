a40c0ae32467e4dadcb0d666fa6f62c05c2fc85f
cihatata <omercihatataa@gmail.com>
2021-01-08 23:25:08 +0300

    feature search film

--- index.js
+++ index.js
@@ -1,23 +1,30 @@
 
-const FILM_API = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=3fd2be6f0c70a2a598f084ddfb75487c&page=1'
+const FILM_API = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=3fd2be6f0c70a2a598f084ddfb75487c&page='
 const IMG_PATH = 'https://image.tmdb.org/t/p/w500';
+const SEARCH_API = 'https://api.themoviedb.org/3/search/movie?api_key=3fd2be6f0c70a2a598f084ddfb75487c&query='
 let filmData;
-const getFilm = async () => {
-  const rawResponse = await fetch(`${FILM_API}`);
+
+const getFilm = async (api) => {
+  const rawResponse = await fetch(`${api}`);
   const data = await rawResponse.json();
   filmData = data.results;
+  renderCard(filmData);
 }
 
 const recFilm = async () => {
-  await getFilm();
-  const filmEl = filmData.map((film) => {
+  //await getFilm(FILM_API);
+  getFilm(FILM_API);
+}
+
+const renderCard = (films) => {
+  const filmEl = films.map((film) => {
     return (`
     <div class="col-lg-4 col-md-4 col-sm-6 mt-5 movie">
 
     <div class="flip-card">
       <div class="flip-card-inner">
         <div class="flip-card-front">
-            <img src="${IMG_PATH}${film.backdrop_path}" class="card-img-top" alt="...">
+            <img src="${IMG_PATH}${film.poster_path}" class="card-img-top" alt="...">
             <div class="card-body card">
               <h5 class="card-title text-light">${film.original_title}</h5>
               </div>
@@ -30,9 +37,10 @@ const recFilm = async () => {
             </div>
             </div>
           </div>
-       ` )
+       `)
   })
   const rootEl = document.querySelector("#root");
+  rootEl.innerHTML = ""
   filmEl.forEach(film => rootEl.innerHTML += film)
 }
 
@@ -52,5 +60,50 @@ chk.addEventListener('change', () => {
   })
 });
 recFilm();
+
+
+const searchEl = document.querySelector('#formSearch');
+const searchValue = document.querySelector('#search');
+console.log(searchEl);
+searchEl.addEventListener('submit', (e) => {
+  e.preventDefault();
+  if (searchValue.value) {
+    getFilm(`${SEARCH_API}${searchValue.value}`);
+  } else {
+    alert('Lutfen bir film ismi giriniz')
+  }
+})
+
+searchValue.addEventListener('change', (e) => {
+  if(!(e.target.value)) {
+    getFilm(FILM_API);
+  }
+})
+const select = () => {
+
+}
+
+let currentPage = 0;
+document.querySelector('#page1').addEventListener('click', () => {
+  getFilm(`${FILM_API}${index++}`)
+})
+document.querySelector('#page2').addEventListener('click', () => {
+  getFilm(`${FILM_API}${index++}`)
+})
+document.querySelector('#page3').addEventListener('click', () => {
+  getFilm(`${FILM_API}${index++}`)
+})
+
+
+const pageList = document.querySelectorAll('.page-link')
+pageList.forEach((item, index) => {
+  // item.onclick = () => {
+  //   getFilm(`${FILM_API}${index}`)
+  // }
+  document.querySelector(`page${index++}`).addEventListener('click', () => {
+  getFilm(`${FILM_API}${index++}`)
+})
+})
+
 
 
